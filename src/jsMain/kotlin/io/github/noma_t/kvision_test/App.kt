86d664fa4e48f0db16e96c185b18d7af94d1d9f3
AomiVel <kaitoyoutube@icloud.com>
2025-03-12 19:10:06 +0900

    うまくいかないのでforce add

--- src/jsMain/kotlin/io/github/noma_t/kvision_test/App.kt
+++ src/jsMain/kotlin/io/github/noma_t/kvision_test/App.kt
@@ -4,19 +4,23 @@ import io.kvision.Application
 import io.kvision.CoreModule
 import io.kvision.BootstrapModule
 import io.kvision.BootstrapCssModule
+import io.kvision.core.Background
+import io.kvision.core.Color
 import io.kvision.html.div
 import io.kvision.module
 import io.kvision.panel.root
 import io.kvision.startApplication
+import io.kvision.utils.px
 
 class App : Application() {
     override fun start() {
         root("kvapp") {
             div {
                 +"This page was created by KVision"
-                
+                fontSize = 24.px
+                color = Color.hex(0x0000FF)
+                background = Background(color = Color.hex(0xFFFF00))
             }
-            // TODO
         }
     }
 }
